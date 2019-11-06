e03a69826bcfa5c4cc7f242b0ab2369c4ad755c1
zhangdeshou <xjlady@126.com>
2019-11-06 13:10:26 +0800

    modify-2

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 
-let demo = document.querySelector('#demo')
+let demo = document.querySelector('#html')
 // let n = 1
 let n = 0 //n从0开始，what different?咿刚好字符串下标就是从0开始的
-let str = `你好，我叫张得瘦，这是我的简历` //申明一个字符串 我用了比较好用反引号`` 
+let str = `/* 你好，我叫张得瘦，这是我的简历
+接下来我要加样式了请看 */
+body{
+    color:#ff0000;
+}` //申明一个字符串 我用了比较好用反引号`` 
+
+//如何字符串的回车在html也变成回车（默认的直接压缩为一个空格）？
+//1. str = str.replace('\n', '<br>') // 只能替换一个，第一个换行\n
+//2 .正则表达式表示所有回车，但是会出现<然后消失  以为下一步’<br‘时html解析出了这是一个标签 然后就把标签转义就看到<了 就变成了此处的换行
+str = str.replace(/\n/g, '<br>')
+
+
+
 // demo.innerHTML = n
 // demo.innerHTML = str[n]
 // demo.innerHTML = str.substring((0, n))
-
+let css = document.querySelector('#css')
 
 //setTimeout()只执行一次就结束了
 // setTimeout(() => {
@@ -61,9 +73,11 @@ let step = () => {
             // demo.innerHTML = n
             // demo.innerHTML = str[n]
             demo.innerHTML = str.substring(0, n)
+            // css.innerHTML = `body{color:#ff0000;}`
+            css.innerHTML = str.substring(0, n)
             // console.log(n)  console.log()调试大法
             step()
-        }, 150)
+        }, 250)
     }
     else { }
 }
